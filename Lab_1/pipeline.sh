61a01c2c53860ad4d35ce6cfa1adaaab9dec92cb
andlarionov <avl2709979@gmail.com>
2024-04-26 15:19:33 +0300

    Added pipeline

---
+++ Lab_1/pipeline.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Запуск скрипта создания данных
+python data_creation.py
+
+# Запуск скрипта предобработки данных
+python model_preprocessing.py
+
+# Запуск скрипта обучения модели
+python model_preparation.py
+
+# Запуск скрипта тестирования модели
+python model_testing.py
