271fa2bbbe4abc1a265a8c5f1a636a8bd7f75b7d
andlarionov <avl2709979@gmail.com>
2024-05-01 17:50:13 +0300

    Changed path for dataset

--- Lab_1/data_creation.py
+++ Lab_1/data_creation.py
@@ -45,7 +45,7 @@ if not os.path.exists('test'):
 
 
 # Сохраняем данные в формате CSV
-x_train.to_csv('LAB_1/train/x_train.csv', index=False)
-y_train.to_csv('LAB_1/train/y_train.csv', index=False)
-x_test.to_csv('LAB_1/test/x_test.csv', index=False)
-y_test.to_csv('LAB_1/test/y_test.csv', index=False)
+x_train.to_csv('Lab_1/train/x_train.csv', index=False)
+y_train.to_csv('Lab_1/train/y_train.csv', index=False)
+x_test.to_csv('Lab_1/test/x_test.csv', index=False)
+y_test.to_csv('Lab_1/test/y_test.csv', index=False)
